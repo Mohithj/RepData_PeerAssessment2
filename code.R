255d5935de92b06834d2ad715198ab02f24e6135
Mohith J <mohithmania@gmail.com>
2021-01-10 22:08:30 +0530

    Code changes and Rmd commit.

--- code.R
+++ code.R
@@ -1,6 +1,6 @@
 ## Downloading the file and reading it into the object data.
 
-if (file.exists("StormData.csv") == FALSE){
+if (file.exists("StormData.csv.bz2") == FALSE){
   url<-"https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
   download.file(url = url, destfile = "StormData.csv.bz2", method = "auto")
 }
@@ -71,40 +71,63 @@ data$DAMAGE <- data$PROPDMG + data$CROPDMG
 ## The total injuries/fatalities is calculated for each event type by using tapply().
 ## Only those event types are selected where the total injuries/fatalities exceed 100 as there are many event types.
 
-htot <- sort(tapply(data$HEALTH, factor(data$EVTYPE), sum, na.rm = T), decreasing = TRUE)
+htot <- sort(tapply(data$HEALTH, factor(data$EVTYPE), sum, na.rm = T), 
+             decreasing = TRUE)
 htot <- htot[htot > 100]
 
 ## The average injuries/fatalities is calculated for each event type and only some event types are selected for the same reason as above.
 
-havg <- sort(tapply(data$HEALTH, factor(data$EVTYPE), mean, na.rm = T), decreasing = TRUE)
+havg <- sort(tapply(data$HEALTH, factor(data$EVTYPE), mean, na.rm = T), 
+             decreasing = TRUE)
 havg <- havg[havg > 5]
 
 ## A dataframe is created for plotting a bar plot.
 
-dfhavg <- data.frame(Event = names(havg), Avg.Injury.Fatality = havg, row.names = NULL)
+dfhtot <- data.frame(Event = names(htot), Total.Injury.Fatality = htot,
+                     row.names = NULL)
+dfhavg <- data.frame(Event = names(havg), Avg.Injury.Fatality = havg, 
+                     row.names = NULL)
 
 library(ggplot2)
-phavg <- ggplot(
-  data = dfhavg, 
-  aes(reorder(Event, Avg.Injury.Fatality),
-  Avg.Injury.Fatality)
-) + geom_col() + coord_flip()
+
+phtot <- ggplot(data = dfhtot, aes(Total.Injury.Fatality,
+          reorder(Event, Total.Injury.Fatality))) + geom_col() + 
+  ggtitle("Total Injuries/Fatalities across US \n from 1950 to 2011") + 
+        xlab("Events") + ylab("Total Injuries/Fatalities") +
+              coord_cartesian(xlim = c(0,9000))
+
+phavg <- ggplot(data = dfhavg, aes(reorder(Event, Avg.Injury.Fatality),
+          Avg.Injury.Fatality)) + geom_col() + 
+      ggtitle("Average Injuries/Fatalities across US \n from 1950 to 2011") + 
+          xlab("Events") + ylab("Average Injuries/Fatalities") + coord_flip()
 
 ## The total damage and average damage is calculated for each event type by using tapply().
 ## Selecting only those events exceeding a particular threshold as there are many events to sift through.
 
-dtot <- sort(tapply(data$DAMAGE, factor(data$EVTYPE), sum, na.rm = T), decreasing = TRUE)
+dtot <- sort(tapply(data$DAMAGE, factor(data$EVTYPE), sum, na.rm = T), 
+             decreasing = TRUE)
 highdtot <- dtot[dtot > 1e+109]
-davg <- sort(tapply(data$DAMAGE, factor(data$EVTYPE), mean, na.rm = T), decreasing = TRUE)
+davg <- sort(tapply(data$DAMAGE, factor(data$EVTYPE), mean, na.rm = T), 
+             decreasing = TRUE)
 highdavg <- davg[davg > 1e+107]
 
 ## Creating a dataframe for barplot.
 
-highdavgdf <- data.frame(Event = names(highdavg), Avg.Damage = highdavg, row.names = NULL)
+highdtotdf <- data.frame(Event = names(highdtot), Total.Damage = highdtot, 
+                         row.names = NULL)
+
+highdavgdf <- data.frame(Event = names(highdavg), Avg.Damage = highdavg, 
+                         row.names = NULL)
 
 library(ggplot2)
-highdavgp <- ggplot(
-  data = highdavgdf, 
-  aes(reorder(Event, Avg.Damage),
-      Avg.Damage)
-) + geom_col() + coord_flip()
+
+highdtotp <- ggplot(data = highdtotdf, aes(reorder(Event, Total.Damage),
+              Total.Damage)) + geom_col() + 
+  ggtitle("Total Damages across US \n from 1950 to 2011") + 
+  xlab("Events") + ylab("Total Damages (In USD)") + coord_flip()
+
+highdavgp <- ggplot(data = highdavgdf, aes(reorder(Event, Avg.Damage),
+              Avg.Damage)) + geom_col() + 
+      ggtitle("Average Damage across US \n from 1950 to 2011") + 
+  xlab("Events") + ylab("Average Damage (In USD)") + coord_flip()
+
